2ddeb131891dc59c6085676066a6b1d5c7f771d3
Marek <s.marekszwed@gmail.com>
2023-12-24 14:11:20 +0100

    done mobile version

---
+++ src/ts/app.ts
@@ -0,0 +1,19 @@
+const billPrice: HTMLInputElement = document.querySelector('.bill__box--price');
+const peopleNumber: HTMLInputElement = document.querySelector('.people__box--price');
+const totalPrice: HTMLParagraphElement = document.querySelector('.amount-price');
+const amountPrice: HTMLParagraphElement = document.querySelector('.total-price');
+const discountBtn = document.querySelectorAll('.discount');
+const customBtn: HTMLInputElement = document.querySelector('.tip__btn-second--custom')
+const resetBtn: HTMLButtonElement = document.querySelector('.reset-btn');
+const app: HTMLElement = document.querySelector('.app')
+
+
+
+
+
+
+
+
+
+    
+
