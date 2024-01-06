424fa43181eb9260bb0f7f17335660f0f1506f68
Marek <s.marekszwed@gmail.com>
2024-01-06 13:09:59 +0100

    Finish function responsible for counting tips

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,36 +7,53 @@ const customInput = document.querySelector('#custom');
 const amountTip = document.querySelector('.amount-price');
 const totalTip = document.querySelector('.total-price');
 // Buttons
-const buttonsPercent = document.querySelectorAll('.discount');
+const PercentButtons = document.querySelectorAll('.discount');
 const resetBtn = document.querySelector('.reset-btn');
+
 // errors
-const outlineError = document.querySelector('.error');
-const errors = document.querySelectorAll('.text-error');
-const peopleError = document.querySelector('.people-error');
-const billError = document.querySelector('.bill-error');
+// const outlineError = document.querySelector('.error');
+// const errors = document.querySelectorAll('.text-error');
+// const peopleError = document.querySelector('.people-error');
+// const billError = document.querySelector('.bill-error');
 
-function test() {
-	const btnTip = parseFloat(this.value);
-	console.log(btnTip);
-}
+PercentButtons.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		PercentButtons.forEach((btnTip) => {
+			btnTip.classList.remove('show');
+		});
 
-// calculate total and amount tip
+		if (e.target.classList.contains('discount')) {
+			e.target.classList.add('show');
+		} else {
+			e.target.classList.remove('show');
+			countBill()
+		}
+		countBill()
+		
+	});
+});
 
-const countBill = () => {
-	let billValue = parseFloat(billInput.value);
-	let numberOfPeople = parseFloat(peopleInput.value);
-	let customPercentage = parseFloat(customInput.value) / 100;
+function countBill() {
+	
+	const billValue = parseFloat(billInput.value);
+	const numberOfPeople = parseFloat(peopleInput.value);
+	const customInputPercentage = parseFloat(customInput.value);
 
-	console.log(billValue);
-	console.log(numberOfPeople);
-	console.log(customPercentage);
+	let btnActive = parseFloat(document.querySelector('button.show').value);
+	
 
-	const totalPrice = customPercentage * billValue.toFixed(2);
-	const priceAmountPerPerson = totalPrice / numberOfPeople;
+	if (customInputPercentage) {
+		btnActive = parseFloat(document.querySelector('#custom').value);
+		// zmienić btn active na odpowiednią klasę
+	}
 
-	totalTip.textContent = `$${totalPrice}`;
+	
+	const totalPrice = (btnActive / 100) * billValue.toFixed(2);
+	const priceAmountPerPerson = (totalPrice / numberOfPeople).toFixed(2);
+
+	totalTip.innerHTML = `$${totalPrice}`;
 	amountTip.innerHTML = `$${priceAmountPerPerson}`;
-};
+}
 
 // AddEventListeners
 resetBtn.addEventListener('click', () => {
@@ -52,52 +69,3 @@ billInput.addEventListener('keyup', countBill);
 peopleInput.addEventListener('keyup', countBill);
 
 customInput.addEventListener('keyup', countBill);
-
-buttonsPercent.forEach((btn) => {
-	btn.addEventListener('click', test);
-});
-
-// const button = buttons.forEach(button => {button.addEventListener('click', () => {
-//     const newButton = parseFloat(button.innerHTML)
-//     console.log(newButton / 100);
-// })})
-
-// allInputs.forEach((input) => {
-//     input.addEventListener('keyup', (e) => {
-//         errors.forEach((error) => {
-//             if (billInput.value == '' || peopleInput.value == '') {
-//                 error.classList.add('active');
-//                 error.classList.add('active');
-//             }
-//             else if (billInput.value !== '' || peopleInput.value == '') {
-//                 error.classList.remove('active');
-//                 error.classList.add('active');
-//             }
-//             else if (billInput.value == '' || peopleInput.value !== '') {
-//                 error.classList.add('active');
-//                 error.classList.remove('active');
-//             }
-//             else if (billInput.value !== '' || peopleInput.value !== '') {
-//                 error.classList.remove('active');
-//                 error.classList.remove('active');
-//             }
-//         });
-//         if (billInput.value == '' || peopleInput.value == '') {
-//             billError.classList.add('active');
-//             peopleError.classList.add('active');
-//         }
-//         else if (billInput.value !== '' || peopleInput.value == '') {
-//             billError.classList.remove('active');
-//             peopleError.classList.add('active');
-//         }
-//         else if (billInput.value == '' || peopleInput.value !== '') {
-//             billError.classList.add('active');
-//             peopleError.classList.remove('active');
-//         }
-//         else if (billInput.value !== '' || peopleInput.value !== '') {
-//             billError.classList.remove('active');
-//             peopleError.classList.remove('active');
-//         }
-//         console.log(e.target);
-//     });
-// });
