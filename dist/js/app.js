a19d2123a7fe0c62babcffa566447e3e65ad9ae2
Marek <s.marekszwed@gmail.com>
2024-01-06 21:37:47 +0100

    Added and fixed bugs in typescript

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,70 +2,55 @@
 const billInput = document.querySelector('#billPrice');
 const peopleInput = document.querySelector('#peopleNumber');
 const customInput = document.querySelector('#custom');
-
-// Right side - paragraphs
-const amountTip = document.querySelector('.amount-price');
-const totalTip = document.querySelector('.total-price');
 // Buttons
 const PercentButtons = document.querySelectorAll('.discount');
 const resetBtn = document.querySelector('.reset-btn');
-
+// Right side 
+const amountTip = document.querySelector('.amount-price');
+const totalTip = document.querySelector('.total-price');
 // errors
-// const outlineError = document.querySelector('.error');
-// const errors = document.querySelectorAll('.text-error');
-// const peopleError = document.querySelector('.people-error');
-// const billError = document.querySelector('.bill-error');
-
+const outlineError = document.querySelector('.error');
+const errors = document.querySelectorAll('.text-error');
+const peopleError = document.querySelector('.people-error');
+const billError = document.querySelector('.bill-error');
+// active class "show" on each button
 PercentButtons.forEach((btn) => {
-	btn.addEventListener('click', (e) => {
-		PercentButtons.forEach((btnTip) => {
-			btnTip.classList.remove('show');
-		});
-
-		if (e.target.classList.contains('discount')) {
-			e.target.classList.add('show');
-		} else {
-			e.target.classList.remove('show');
-			countBill()
-		}
-		countBill()
-		
-	});
+    btn.addEventListener('click', (e) => {
+        PercentButtons.forEach((btnTip) => {
+            btnTip.classList.remove('show');
+        });
+        if (e.target.classList.contains('discount')) {
+            e.target.classList.add('show');
+        }
+        else {
+            e.target.classList.remove('show');
+            countBill();
+        }
+        countBill();
+    });
 });
-
+// calculate the tip
 function countBill() {
-	
-	const billValue = parseFloat(billInput.value);
-	const numberOfPeople = parseFloat(peopleInput.value);
-	const customInputPercentage = parseFloat(customInput.value);
-
-	let btnActive = parseFloat(document.querySelector('button.show').value);
-	
-
-	if (customInputPercentage) {
-		btnActive = parseFloat(document.querySelector('#custom').value);
-		// zmienić btn active na odpowiednią klasę
-	}
-
-	
-	const totalPrice = (btnActive / 100) * billValue.toFixed(2);
-	const priceAmountPerPerson = (totalPrice / numberOfPeople).toFixed(2);
-
-	totalTip.innerHTML = `$${totalPrice}`;
-	amountTip.innerHTML = `$${priceAmountPerPerson}`;
+    const billValue = parseFloat(billInput.value);
+    const numberOfPeople = parseFloat(peopleInput.value);
+    const customInputPercentage = parseFloat(customInput.value);
+    let btnActive = parseFloat(document.querySelector('button.show').value);
+    if (customInputPercentage) {
+        btnActive = parseFloat(document.querySelector('#custom').value);
+    }
+    const totalPrice = (btnActive / 100) * parseFloat(billValue.toFixed(2));
+    const priceAmountPerPerson = parseFloat((totalPrice / numberOfPeople).toFixed(2));
+    totalTip.innerHTML = `$${totalPrice}`;
+    amountTip.innerHTML = `$${priceAmountPerPerson}`;
 }
-
 // AddEventListeners
 resetBtn.addEventListener('click', () => {
-	billInput.value = '0';
-	peopleInput.value = '0';
-	customInput.value = '';
-	amountTip.innerHTML = '$0.00';
-	totalTip.textContent = '$0.00';
+    billInput.value = '0';
+    peopleInput.value = '0';
+    customInput.value = '';
+    amountTip.innerHTML = '$0.00';
+    totalTip.innerHTML = '$0.00';
 });
-
 billInput.addEventListener('keyup', countBill);
-
 peopleInput.addEventListener('keyup', countBill);
-
 customInput.addEventListener('keyup', countBill);
---
+++ src/ts/app.ts
@@ -0,0 +1,70 @@
+// Inputs
+const billInput: HTMLInputElement = document.querySelector('#billPrice');
+const peopleInput: HTMLInputElement = document.querySelector('#peopleNumber');
+const customInput: HTMLInputElement = document.querySelector('#custom');
+// Buttons
+const PercentButtons = document.querySelectorAll('.discount');
+const resetBtn = document.querySelector('.reset-btn');
+// Right side 
+const amountTip = document.querySelector('.amount-price');
+const totalTip = document.querySelector('.total-price');
+
+// errors
+const outlineError = document.querySelector('.error');
+const errors = document.querySelectorAll('.text-error');
+const peopleError = document.querySelector('.people-error');
+const billError = document.querySelector('.bill-error');
+
+// active class "show" on each button
+PercentButtons.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		PercentButtons.forEach((btnTip) => {
+			btnTip.classList.remove('show');
+		});
+
+		if ((e.target as Element).classList.contains('discount')) {
+			(e.target as Element).classList.add('show');
+		} else {
+			(e.target as Element).classList.remove('show');
+			countBill()
+		}
+		countBill()
+		
+	});
+});
+
+// calculate the tip
+function countBill() {
+	
+	const billValue: number = parseFloat(billInput.value);
+	const numberOfPeople: number = parseFloat(peopleInput.value);
+	const customInputPercentage: number = parseFloat(customInput.value);
+	let btnActive: number = parseFloat((document.querySelector('button.show') as HTMLButtonElement).value);
+	
+	if (customInputPercentage) {
+		btnActive = parseFloat((document.querySelector('#custom') as HTMLInputElement).value);
+	}
+
+	const totalPrice: number = (btnActive / 100) * parseFloat(billValue.toFixed(2));
+	const priceAmountPerPerson: number = parseFloat((totalPrice / numberOfPeople).toFixed(2));
+
+	totalTip.innerHTML = `$${totalPrice}`;
+	amountTip.innerHTML = `$${priceAmountPerPerson}`;
+}
+
+
+
+// AddEventListeners
+resetBtn.addEventListener('click', () => {
+	billInput.value = '0';
+	peopleInput.value = '0';
+	customInput.value = '';
+	amountTip.innerHTML = '$0.00';
+	totalTip.innerHTML = '$0.00';
+});
+
+billInput.addEventListener('keyup', countBill);
+
+peopleInput.addEventListener('keyup', countBill);
+
+customInput.addEventListener('keyup', countBill);
